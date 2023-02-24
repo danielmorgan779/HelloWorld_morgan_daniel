2ce2cf38a88eeb939ddf56619dfe9a2364233d5d
danielmorgan779 <danielm462005@gmail.com>
2023-02-24 08:21:10 -0600

    Initial commit

---
+++ HelloWorld_morgan_daniel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld_morgan_daniel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("We demand your first name!");        // This will write to the use asking for their first name
+            string firstName = Console.ReadLine();      // This assigns what they typed in for their firstname to the firstName var
+            Console.WriteLine("Now your last name?");       // this will ask for their last name
+            string lastName = Console.ReadLine();       // and this assigns what they typed for their lastname to the firstName var
+
+            // This displays their first and last name
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
+        }
+    }
+}
